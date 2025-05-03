d229c183076c9a73b1a3a5d20e99aac12ed482b3
Judd Spence <judsonspence1@gmail.com>
2025-05-03 13:34:41 -0400

    Fix package.json and add Next.js dependencies

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,6 @@
-import '../styles/globals.css';
+
+import '../styles/globals.css'
+import { supabase } from '../lib/supabaseClient'
 
 export default function App({ Component, pageProps }) {
   return <Component {...pageProps} />;
